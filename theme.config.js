2618dc820a8aea90f0d0c69b00d70fa64215cab0
linada <2410694+naxiaoduo@users.noreply.github.com>
2023-01-16 14:45:48 +0800

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Na xiaoduo.
+      <time>{YEAR}</time> © Naxiaoduo（拿小多）.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
